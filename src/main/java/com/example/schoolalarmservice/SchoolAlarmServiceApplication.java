a9cd9d89c5d3f978922e5bd594b3b69170a8a05b
taeg2 <90079870+taeg2@users.noreply.github.com>
2026-05-18 17:41:09 +0900

    create school alarm service

---
+++ src/main/java/com/example/schoolalarmservice/SchoolAlarmServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.schoolalarmservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolAlarmServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolAlarmServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/schoolalarmservice/SchoolAlarmServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.schoolalarmservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolAlarmServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
